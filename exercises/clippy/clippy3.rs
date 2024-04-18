a75605e3bcd216f347a89cb433c6bf9506bfe32d
ffl-web <3021593440@qq.com>
2024-04-18 22:15:33 +0800

    2024.04.18

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -4,7 +4,7 @@
 //
 // Execute `rustlings hint clippy3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 #[allow(unused_variables, unused_assignments)]
 fn main() {
@@ -14,18 +14,22 @@ fn main() {
     }
 
     let my_arr = &[
-        -1, -2, -3
+        -1, -2, -3,
         -4, -5, -6
     ];
     println!("My array! Here it is: {:?}", my_arr);
 
-    let my_empty_vec = vec![1, 2, 3, 4, 5].resize(0, 5);
+    let mut my_empty_vec = vec![1, 2, 3, 4, 5];
+    my_empty_vec.clear();
+    my_empty_vec.resize(5, 5);
     println!("This Vec is empty, see? {:?}", my_empty_vec);
 
     let mut value_a = 45;
     let mut value_b = 66;
+    let mut a=0;
     // Let's swap these two!
-    value_a = value_b;
-    value_b = value_a;
+    a=value_b;
+   
+    value_b = value_a; value_a = a;
     println!("value a: {}; value b: {}", value_a, value_b);
 }
